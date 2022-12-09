44802f2a1e4b60f4ca37409cdac164e9c887e935
Alfredo Cardigliano <cardigliano@ntop.org>
2022-12-09 15:18:21 +0000

    Add branding

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ npm run build || exit
 # Install
 rm -rf /usr/share/cockpit/ntopng
 mkdir /usr/share/cockpit/ntopng
+cp -r branding/* /usr/share/cockpit/branding/ubuntu/
 cp -r plugins/ntopng/* /usr/share/cockpit/ntopng/ 
 cp -r dist/* /usr/share/cockpit/ntopng/
